e9a2934a6a1cfb981cd40a50d59e4a369d3c2e4a
Glen Danielle D. Galit <glendanielleg@gmail.com>
2023-08-09 09:55:01 +0800

    try

--- front/src/Components/AboutMe.jsx
+++ front/src/Components/AboutMe.jsx
@@ -24,7 +24,7 @@ const AboutMe = () => {
         <div key={data.mema.id}>
 
           <div>
-            <p>{data.mema.name}</p>
+            <p>{data.mema1.name}</p>
           </div>
 
         </div>
